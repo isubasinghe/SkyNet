de9fade3e1edd68c3b4a273e7760736e9109d00a
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2018-07-29 09:31:40 +1000

    changes made

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,6 +2,8 @@ import atexit
 from datetime import datetime
 import rethinkdb as r
 
+HOST = "192.168.99.100"
+PORT = 32769
 
 def load_model():
     #__rethinkdb = r.connect("192.168.99.100", 32769, db="test")
@@ -9,7 +11,7 @@ def load_model():
     pass
 
 def add_fake_users():
-    conn = r.connect("192.168.99.100", 32769, db="test")
+    conn = r.connect("HOST", PORT, db="test")
     for i in range(10):
         r.table('users').insert([
             {
@@ -22,19 +24,19 @@ def add_fake_users():
     conn.close()
 
 def insert_coords(user, coords, processed):
-    conn = r.connect("192.168.99.100", 32769)
+    conn = r.connect("HOST", PORT)
     print(coords)
     user = r.table('users').filter({"user": user}).update({"location": r.point(coords[0], coords[1]), "time": r.expr(datetime.now(r.make_timezone('+11:00'))), "processed": processed}, return_changes=True).run(conn)
     # print(user)
     conn.close()
 
 def set_true():
-    conn = r.connect("192.168.99.100", 32769)
+    conn = r.connect("HOST", PORT)
     r.table('users').filter({"processed": False}).update({"processed": True}, return_changes=True).run(conn)
     conn.close()
 
 def add_fake_coords():
-    conn = r.connect("192.168.99.100", 32769)
+    conn = r.connect("HOST", PORT)
     coords = [[144.9738,-37.8110], [144.9671,-37.8183], [144.9561, -37.8117], [144.9676, -37.8118], [144.9764, -37.8101],[144.9463,-37.8112],[144.9717,-37.8033],[144.9568,-37.8076],[144.9583,-37.8206]]
     for i in range(len(coords)):
         r.table('users').filter({"user": "user_" + str(i+1)}).update({"location": r.point(coords[i][0], coords[i][1]), "time": r.expr(datetime.now(r.make_timezone("+11:00"))), "processed": False}, return_changes=True).run(conn)
@@ -44,7 +46,7 @@ def add_fake_coords():
 
 
 def get_user_coords(user):
-    conn = r.connect("192.168.99.100", 32769)
+    conn = r.connect("HOST", PORT)
     cursor = r.table('users').filter({"user": user}).run(conn)
     for document in cursor:
 
@@ -55,7 +57,7 @@ def get_user_coords(user):
     return retval    
 
 def get_coords():
-    conn = r.connect("192.168.99.100", 32769)
+    conn = r.connect("HOST", PORT)
     # Obtain the unprocessed users
     cursor = r.table("users").filter({"processed": False}).run(conn)
     #cursor = r.table("users").run(conn)
